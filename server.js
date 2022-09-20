05d761375e1e8f2b3cb079354704b12973013463
Orelbi Acosta <orelbi088@gmail.com>
2022-09-20 02:11:34 -0400

    test2

--- server.js
+++ server.js
@@ -10,11 +10,17 @@ const app = express();
 
 const server = http.createServer(app);
 const io = socketio(server, {
-    cors: {
-        origin: '*',
-    },
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
 });
-io.set('origins', '*:*');
+
 
 io.on('connection', (socket) => {
     socket.on('join', ({ gameId }) => {
