2da509afe0ae90874d64e6fa8f8b95ffef849e5a
Orelbi Acosta <orelbi088@gmail.com>
2022-09-20 03:34:01 -0400

    test

--- server.js
+++ server.js
@@ -1,67 +1,57 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const socketio = require('socket.io');
-const http = require('http');
-const path = require('path');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const socketio = require("socket.io");
+const http = require("http");
+const path = require("path");
+require("dotenv").config();
 
 const app = express();
 
 const server = http.createServer(app);
-const io = socketio(server, {
-    handlePreflightRequest: (req, res) => {
-        const headers = {
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-            "Access-Control-Allow-Credentials": true
-        };
-        res.writeHead(200, headers);
-        res.end();
-    }
-});
-io.set('origins', 'https://chess-wine-mu.vercel.app:80');
-io.on('connection', (socket) => {
-    socket.on('join', ({ gameId }) => {
-        socket.join(gameId);
-        io.to(gameId).emit('userJoined');
-    });
-    socket.on('postMove', (data) => {
-        io.to(data.gameId).emit('receiveMove', data);
-    });
+const io = socketio(server, { cors: { origin: "*" } });
+
+io.on("connection", (socket) => {
+  socket.on("join", ({ gameId }) => {
+    socket.join(gameId);
+    io.to(gameId).emit("userJoined");
+  });
+  socket.on("postMove", (data) => {
+    io.to(data.gameId).emit("receiveMove", data);
+  });
 });
 
 app.use(express.json());
 app.use(cors());
 
 // setting the routers
-const gameRouter = require('./routes/gameRouter');
-const userRouter = require('./routes/userRouter');
-app.use('/', (req, res)=>{
-    res.json('Chess API');
+const gameRouter = require("./routes/gameRouter");
+const userRouter = require("./routes/userRouter");
+app.use("/", (req, res) => {
+  res.json("Chess API");
 });
-app.use('/g', gameRouter);
-app.use('/u', userRouter);
+app.use("/g", gameRouter);
+app.use("/u", userRouter);
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 // connect to mongodb database
 mongoose.connect(
-    process.env.MONGO_URI,
-    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
-    (err) => {
-        if (err) throw err;
-        console.log('MongoDB is connected');
-    }
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+  (err) => {
+    if (err) throw err;
+    console.log("MongoDB is connected");
+  }
 );
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
-    console.log(`Server is up and running at port: ${port}`);
+  console.log(`Server is up and running at port: ${port}`);
 });
